f49833ed9072002ffb0ce7f8f78a09fdbc8f11d7
Rustam Qurbanov <rustam.qurbanov02@gmail.com>
2024-02-28 01:57:17 +0400

    create react todo app

---
+++ src/components/search-panel.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SearchPanel = () => {
+  const searchtext = "Search";
+
+  return <input placeholder={searchtext} />;
+};
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import TodoList from "./components/todo-list";
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const todoData = [
+    { label: "Drink Coffee", important: false },
+    { label: "Make Awesome App", important: false },
+    { label: "Have a lunch", important: false },
+  ];
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
