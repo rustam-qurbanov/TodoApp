70558a1969b1ee902be8a809bb6675cad9a8c491
Rustam Qurbanov <rustam.qurbanov02@gmail.com>
2024-03-02 17:23:23 +0400

    add click event for todo-list-item and item-status-filter

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,47 +1,110 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./todo-list-item.css";
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "steelblue" : "black",
-    fontWeight: important ? "bold" : "normal",
+export default class TodoListItem extends Component {
+  // constructor() {
+  //   super();
+
+  //   this.onLabelClick = () => {
+  //     console.log(`Done: ${this.props.label}`);
+  //   };
+  // }
+
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`);
   };
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
-        {label}
-      </span>
+  render() {
+    const { label, important = false } = this.props;
 
-      <div>
-        <button
-          type="button"
-          className="btn btn-outline-success btn-sm float-right"
-        >
-          <i className="fa fa-exclamation" />
-        </button>
+    const style = {
+      color: important ? "steelblue" : "black",
+      fontWeight: important ? "bold" : "normal",
+    };
 
-        <button
-          type="button"
-          className="btn btn-outline-danger btn-sm float-right"
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this.onLabelClick}
         >
-          {/* <i className="fa fa-trash-o" /> */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="15"
-            height="15"
-            fill="currentColor"
-            class="bi bi-trash"
-            viewBox="0 0 16 16"
+          {label}
+        </span>
+
+        <div>
+          <button
+            type="button"
+            className="btn btn-outline-success btn-sm float-right"
           >
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-          </svg>
-        </button>
-      </div>
-    </span>
-  );
-};
+            <i className="fa fa-exclamation" />
+          </button>
 
-export default TodoListItem;
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm float-right"
+          >
+            {/* <i className="fa fa-trash-o" /> */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              fill="currentColor"
+              className="bi bi-trash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+            </svg>
+          </button>
+        </div>
+      </span>
+    );
+  }
+}
+
+// export default TodoListItem;
+
+// const TodoListItemFunc = ({ label, important = false }) => {
+//   const style = {
+//     color: important ? "steelblue" : "black",
+//     fontWeight: important ? "bold" : "normal",
+//   };
+
+//   return (
+//     <span className="todo-list-item">
+//       <span className="todo-list-item-label" style={style}>
+//         {label}
+//       </span>
+
+//       <div>
+//         <button
+//           type="button"
+//           className="btn btn-outline-success btn-sm float-right"
+//         >
+//           <i className="fa fa-exclamation" />
+//         </button>
+
+//         <button
+//           type="button"
+//           className="btn btn-outline-danger btn-sm float-right"
+//         >
+//           {/* <i className="fa fa-trash-o" /> */}
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             width="15"
+//             height="15"
+//             fill="currentColor"
+//             className="bi bi-trash"
+//             viewBox="0 0 16 16"
+//           >
+//             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+//             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+//           </svg>
+//         </button>
+//       </div>
+//     </span>
+//   );
+// };
+// export default TodoListItemFunc;
