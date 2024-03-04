07f4af16971a587bf50d37aef35e887b73a9236e
Rustam Qurbanov <rustam.qurbanov02@gmail.com>
2024-03-05 00:12:24 +0400

    add func removing and adding an element

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,91 @@
+import React, { Component } from "react";
+
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
 import ItemStatusFilter from "../item-status-filter";
+import ItemAddForm from "../item-add-form";
 
 import "./app.css";
 
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Make Awesome App", important: true, id: 2 },
-    { label: "Have a lunch", important: false, id: 3 },
-  ];
+export default class App extends Component {
+  maxId = 100;
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    todoData: [
+      { label: "Drink Coffee", important: false, id: 1 },
+      { label: "Make Awesome App", important: true, id: 2 },
+      { label: "Have a lunch", important: false, id: 3 },
+    ],
+  };
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArr = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
+      return {
+        todoData: newArr,
+      };
+    });
+  };
+
+  addItem = (text) => {
+
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++,
+    };
+
+
+    this.setState(({ todoData }) => {
+      const newArr = [...todoData, newItem];
+
+      return {
+        todoData: newArr
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
+    );
+  }
+}
 
-      <TodoList 
-        todos={todoData}
-        onDeleted={ (id) => console.log("del", id)}
-      />
-    </div>
-  );
-};
+// const App = () => {
+//   const todoData = [
+//     { label: "Drink Coffee", important: false, id: 1 },
+//     { label: "Make Awesome App", important: true, id: 2 },
+//     { label: "Have a lunch", important: false, id: 3 },
+//   ];
 
-export default App;
+//   return (
+//     <div className="todo-app">
+//       <AppHeader toDo={1} done={3} />
+//       <div className="top-panel d-flex">
+//         <SearchPanel />
+//         <ItemStatusFilter />
+//       </div>
+
+//       <TodoList
+//         todos={todoData}
+//         onDeleted={ (id) => console.log("del", id)}
+//       />
+//     </div>
+//   );
+// };
+// export default App;
---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import "./item-add-form.css";
+
+class ItemAddForm extends Component {
+  render() {
+    return (
+      <div className="item-add-form">
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => this.props.onItemAdded("Hello World")}
+        >
+          Add Item
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ItemAddForm;
